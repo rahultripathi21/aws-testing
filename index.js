4e37b61c26383e6d4cd3e9e4bf438f2123190c3a
Rahul Tripathi <58801441+rahultripathi21@users.noreply.github.com>
2024-05-20 00:50:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("hello");
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('I am up');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
